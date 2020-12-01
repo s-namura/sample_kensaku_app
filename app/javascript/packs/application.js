3ea5705052c5f461e5aa99e047646ec7d89c1c59
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-12-01 19:15:16 +0900

    Bootstrap・jQueryの設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//require("jquery")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("bootstrap/dist/js/bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +15,13 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+document.addEventListener("turbolinks:load", () => {
+  $(function () {
+    $("h1").css("color", "#0000FF");
+  });
+
+  
+
+})
